981ff38384f4be94b648bf59338a8d31b9eca58d
Nano <gaomingzhao666@outlook.com>
2026-02-07 16:15:16 +0900

    add main command definition and run function for application entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-//  nothing
+import { defineCommand, runMain } from 'citty'
+import { name, version, description } from '../package.json'
+
+import infoCommand from './commands/info'
+
+export const main = defineCommand({
+	meta: {
+		name,
+		version,
+		description,
+	},
+
+	subCommands: {
+		info: infoCommand,
+	},
+})
+
+export async function run() {
+	await runMain(main)
+}

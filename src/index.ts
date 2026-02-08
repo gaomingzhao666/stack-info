97c12220a657eee8d570758b1bf29829d029dc6d
Nano <gaomingzhao666@outlook.com>
2026-02-08 16:11:29 +0900

    fix: correct description formatting in main command metadata

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const main = defineCommand({
 		name: 'stack-info',
 		version: '1.0.0',
 		description:
-			'"A cross-platform, framework-agnostic CLI for inspecting web project stacks in command line.',
+			'A cross-platform, framework-agnostic CLI for inspecting web project stacks in command line.',
 	},
 
 	subCommands: {

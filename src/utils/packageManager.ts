d95e83eea61a73ed37edddc00880e84358f05b03
Nano <gaomingzhao666@outlook.com>
2026-02-08 15:39:58 +0900

    fix: simplify getPackageManagerVersion function definition by using arrow function

--- src/utils/packageManager.ts
+++ src/utils/packageManager.ts
@@ -1,5 +1,4 @@
 import { execSync } from 'node:child_process'
 
-export function getPackageManagerVersion(name: string) {
-	return execSync(`${name} --version`).toString('utf8').trim()
-}
+export const getPackageManagerVersion = (name: string) =>
+	execSync(`${name} --version`).toString('utf8').trim()

cb15a24238add490d34ac6a45ae8ddbfc355d120
Nano <gaomingzhao666@outlook.com>
2026-02-08 15:39:28 +0900

    fix: simplify getStringWidth and formatInfoBox function definitions by using arrow functions and removing unnecessary line breaks

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -2,20 +2,15 @@ import process from 'node:process'
 import { stripVTControlCharacters } from 'node:util'
 import { colors } from 'consola/utils'
 
-function getStringWidth(str: string): number {
+const getStringWidth = (str: string): number => {
 	const stripped = stripVTControlCharacters(str)
 	let width = 0
 
 	for (const char of stripped) {
 		const code = char.codePointAt(0)
-		if (!code) {
-			continue
-		}
-
+		if (!code) continue
 		// Variation selectors don't add width
-		if (code >= 0xfe00 && code <= 0xfe0f) {
-			continue
-		}
+		if (code >= 0xfe00 && code <= 0xfe0f) continue
 
 		// Emoji and wide characters (simplified heuristic)
 		// Most emojis are in these ranges
@@ -27,19 +22,17 @@ function getStringWidth(str: string): number {
 			(code >= 0x2700 && code <= 0x27bf) || // Dingbats
 			(code >= 0x1f900 && code <= 0x1f9ff) || // Supplemental symbols and pictographs
 			(code >= 0x1fa70 && code <= 0x1faff) // Symbols and Pictographs Extended-A
-		) {
+		)
 			width += 2
-		} else {
-			width += 1
-		}
+		else width += 1
 	}
 
 	return width
 }
 
-export function formatInfoBox(
+export const formatInfoBox = (
 	infoObj: Record<string, string | undefined>,
-): string {
+): string => {
 	let firstColumnLength = 0
 	let ansiFirstColumnLength = 0
 	const entries = Object.entries(infoObj).map(([label, val]) => {
@@ -77,9 +70,8 @@ export function formatInfoBox(
 
 			if (boxRowLength + wordLength + spaceLength > terminalWidth) {
 				// Wrap to next line
-				if (currentLine) {
-					boxStr += colors.cyan(currentLine)
-				}
+				if (currentLine) boxStr += colors.cyan(currentLine)
+
 				boxStr += `\n${' '.repeat(firstColumnLength)}`
 				currentLine = word
 				boxRowLength = firstColumnLength + wordLength
@@ -88,10 +80,7 @@ export function formatInfoBox(
 				boxRowLength += wordLength + spaceLength
 			}
 		}
-
-		if (currentLine) {
-			boxStr += colors.cyan(currentLine)
-		}
+		if (currentLine) boxStr += colors.cyan(currentLine)
 
 		boxStr += '\n'
 	}
